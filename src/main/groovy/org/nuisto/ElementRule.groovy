d11fe1502ff9087e563ceb7219dd47173e783c3f
Chad Gorshing <gorshing@gmail.com>
2017-08-14 13:04:47 -0500

    Format/cleanup

--- src/main/groovy/org/nuisto/ElementRule.groovy
+++ src/main/groovy/org/nuisto/ElementRule.groovy
@@ -4,56 +4,56 @@ import groovy.util.logging.Slf4j
 
 @Slf4j(category = 'org.nuisto.mat')
 class ElementRule {
-    ElementRule() {
-        println 'In constructor*****'
-    }
+  ElementRule() {
+    println 'In constructor*****'
+  }
 
-    ElementRule version(String version) {
-        println 'Parsing a file with the version of ' + version
+  ElementRule version(String version) {
+    println 'Parsing a file with the version of ' + version
 
-        return this
-    }
+    return this
+  }
 
-    ElementRule element(String name) {
-        println 'Picked up element of ' + name
+  ElementRule element(String name) {
+    println 'Picked up element of ' + name
 
-        return this
-    }
+    return this
+  }
 
-    ElementRule hasAttribute(String name) {
-        println 'Something should have attribute of ' + name
+  ElementRule hasAttribute(String name) {
+    println 'Something should have attribute of ' + name
 
-        return this
-    }
+    return this
+  }
 
-    ElementRule hasParent(String name) {
-        println 'It should have a parent of ' + name
+  ElementRule hasParent(String name) {
+    println 'It should have a parent of ' + name
 
-        return this
-    }
+    return this
+  }
 
-    ElementRule hasChild(String name) {
-        println 'It should have child of ' + name
+  ElementRule hasChild(String name) {
+    println 'It should have child of ' + name
 
-        return this
-    }
+    return this
+  }
 
-    ElementRule withAttribute(String name) {
-        println 'It should have attribute of ' + name
+  ElementRule withAttribute(String name) {
+    println 'It should have attribute of ' + name
 
-        return this
-    }
+    return this
+  }
 
-    ElementRule withValue(String name) {
-        println 'It should have value of ' + name
+  ElementRule withValue(String name) {
+    println 'It should have value of ' + name
 
-        return this
-    }
+    return this
+  }
 
-    ElementRule havingValue(String name) {
-        println 'havingValue of ' + name
+  ElementRule havingValue(String name) {
+    println 'havingValue of ' + name
 
-        return this
-    }
+    return this
+  }
 }
 
