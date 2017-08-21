6c470c634fa4977433cbb6a380f3730ff57b8174
Chad Gorshing <gorshing@gmail.com>
2017-08-20 19:02:10 -0500

    Wiring the builder to the expectations

--- src/main/groovy/org/nuisto/ExpectationBuilder.groovy
+++ src/main/groovy/org/nuisto/ExpectationBuilder.groovy
@@ -2,9 +2,10 @@ package org.nuisto
 
 import groovy.util.logging.Slf4j
 
-@Slf4j
+@Slf4j(category = 'org.nuisto.msa')
 class ExpectationBuilder {
-  def expectations = new ArrayList<Expectation>()
+  Expectation currentExpectation
+  List<Expectation> expectations = new ArrayList<Expectation>()
 
   ExpectationBuilder() {
   }
@@ -12,11 +13,21 @@ class ExpectationBuilder {
   void element(String name) {
     Expectation expectation = new Expectation(elementName: name)
 
+    currentExpectation = expectation
+
     expectations << expectation
   }
 
-  void hasAttribute(String s) {
+  void hasAttribute(String attribute) {
+    currentExpectation.hasAttribute(attribute)
+  }
 
+  void hasAttribute(String attribute, String value) {
+    currentExpectation.hasAttribute(attribute, value)
+  }
+
+  void hasAttribute(String attribute, List<String> values) {
+    currentExpectation.hasAttribute(attribute, values)
   }
 
   List<Expectation> getExpectations() {
