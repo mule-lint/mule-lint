b7a1204d1108d1497a4896aaeeb4ecc4b34a2ac9
Chad Gorshing <gorshing@gmail.com>
2017-09-05 05:17:20 -0500

    Organized

--- src/main/groovy/org/nuisto/LoaderBaseScriptClass.groovy
+++ src/main/groovy/org/nuisto/LoaderBaseScriptClass.groovy
@@ -4,16 +4,16 @@ import groovy.util.logging.Slf4j
 
 @Slf4j(category = 'org.nuisto.msa')
 abstract class LoaderBaseScriptClass extends Script {
-  ElementRule element(String name) {
-    return this.binding.elementRule.element(name)
-  }
-
   ElementRule version(String version) {
     this.binding.elementRule.version(version)
 
     this.binding.elementRule = new ElementRule(this.binding.elementRule.builder)
 
     return this.binding.elementRule
+  }
+
+  ElementRule element(String name) {
+    return this.binding.elementRule.element(name)
   }
 }
 
