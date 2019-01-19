b78ef4be03b04c6c057bb6184ac3caad8ce93d54
Chad Gorshing <gorshing@gmail.com>
2019-01-19 17:08:27 -0600

    Need to work on RunnerTests

--- src/test/groovy/org/nuisto/RunnerTests.groovy
+++ src/test/groovy/org/nuisto/RunnerTests.groovy
@@ -1,12 +1,23 @@
 package org.nuisto
 
+import org.junit.Ignore
 import org.junit.Test
+import org.nuisto.model.OptionsModel
 
 class RunnerTests {
+  @Ignore
   @Test
-  void foo() {
-    Runner runner = new Runner()
+  void infractionsAreTalliedUpCorrectlyAndNotDuplicated() {
+    Runner runner = new Runner(new DummyResultsHandler())
 
-   // runner.runWithModel()
+    OptionsModel model = new OptionsModel( sourceDirectory: 'src/test/resources/samples', rules: 'src/test/resources/samples/rules.txt')
+
+    int result = runner.runWithModel(model)
+
+    //I have found infractions where they keep getting outputted repeatedly.
+    //Write a test and get this to fail, then fix it
+    //Update: This doesn't seem to be the case anymore. But I'll leave this test here as something to build upon.
+    //Do need to create tests around the runner
+    assert 0 == result
   }
 }
