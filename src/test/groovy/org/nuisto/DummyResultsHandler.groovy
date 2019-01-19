cec75471fe2550e06745732a05d79d9c9de4137d
Chad Gorshing <gorshing@gmail.com>
2019-01-19 17:07:52 -0600

    Added DummyResultsHandler

---
+++ src/test/groovy/org/nuisto/DummyResultsHandler.groovy
@@ -0,0 +1,9 @@
+package org.nuisto
+
+import org.nuisto.model.OptionsModel
+import org.nuisto.model.ResultsModel
+
+class DummyResultsHandler extends ResultsHandler {
+  void handleResults(OptionsModel optionsModel, ResultsModel resultsModel) {
+  }
+}
