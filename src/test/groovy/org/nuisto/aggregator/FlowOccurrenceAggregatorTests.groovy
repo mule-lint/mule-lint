eeacc011b50d37270b314f85a0b6b6c2d1dbf454
Chad Gorshing <gorshing@gmail.com>
2019-01-27 09:07:07 -0600

    Added aggregator tests

---
+++ src/test/groovy/org/nuisto/aggregator/FlowOccurrenceAggregatorTests.groovy
@@ -0,0 +1,71 @@
+package org.nuisto.aggregator
+
+import org.junit.Before
+import org.junit.Test
+import org.nuisto.MuleXmlNode
+import org.nuisto.NodeChecker
+
+class FlowOccurrenceAggregatorTests {
+  FlowOccurrenceAggregator aggregator
+  NodeChecker nodeChecker
+
+  @Before
+  void setup() {
+    aggregator = new FlowOccurrenceAggregator()
+    nodeChecker = new NodeChecker(['core': 'wqer', 'http': 'http://www.mulesoft.org/schema/mule/http'])
+  }
+
+  @Test
+  void muleFileHasNoFlowReferences() {
+
+    def root = new XmlParser().parseText('''
+<mule xmlns:http="http://www.mulesoft.org/schema/mule/http"
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xsi:schemaLocation="
+http://www.mulesoft.org/schema/mule/http http://www.mulesoft.org/schema/mule/http/current/mule-http.xsd">
+  <http:request />
+  <logger />
+</mule>
+''')
+
+    aggregator.handleNode(new MuleXmlNode(root.children()[0], nodeChecker))
+
+    assert aggregator.totals.get('flowCount') == 0
+  }
+
+  @Test
+  void muleFileHasSingleFlowReference() {
+
+    def root = new XmlParser().parseText('''
+<mule xmlns:http="http://www.mulesoft.org/schema/mule/http"
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xsi:schemaLocation="
+http://www.mulesoft.org/schema/mule/http http://www.mulesoft.org/schema/mule/http/current/mule-http.xsd">
+  <flow />
+</mule>
+''')
+
+    aggregator.handleNode(new MuleXmlNode(root.children()[0], nodeChecker))
+
+    assert aggregator.totals.get('flowCount') == 1
+  }
+
+  @Test
+  void muleFileHasSubFlowReferences() {
+
+    def root = new XmlParser().parseText('''
+<mule xmlns:http="http://www.mulesoft.org/schema/mule/http"
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xsi:schemaLocation="
+http://www.mulesoft.org/schema/mule/http http://www.mulesoft.org/schema/mule/http/current/mule-http.xsd">
+  <flow />
+  <sub-flow />
+</mule>
+''')
+
+    aggregator.handleNode(new MuleXmlNode(root.children()[0], nodeChecker))
+    aggregator.handleNode(new MuleXmlNode(root.children()[1], nodeChecker))
+
+    assert aggregator.totals.get('flowCount') == 2
+  }
+}
---
+++ src/test/groovy/org/nuisto/aggregator/LoggerOccurrenceAggregatorTests.groovy
@@ -0,0 +1,51 @@
+package org.nuisto.aggregator
+
+import org.junit.Before
+import org.junit.Test
+import org.nuisto.MuleXmlNode
+import org.nuisto.NodeChecker
+
+class LoggerOccurrenceAggregatorTests {
+  LoggerOccurrenceAggregator aggregator
+  NodeChecker nodeChecker
+
+  @Before
+  void setup() {
+    aggregator = new LoggerOccurrenceAggregator()
+    nodeChecker = new NodeChecker(['core': 'wqer', 'http': 'http://www.mulesoft.org/schema/mule/http'])
+  }
+
+  @Test
+  void muleFileHasNoLoggerReferences() {
+
+    def root = new XmlParser().parseText('''
+<mule xmlns:http="http://www.mulesoft.org/schema/mule/http"
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xsi:schemaLocation="
+http://www.mulesoft.org/schema/mule/http http://www.mulesoft.org/schema/mule/http/current/mule-http.xsd">
+  <http:request />
+</mule>
+''')
+
+    aggregator.handleNode(new MuleXmlNode(root.children()[0], nodeChecker))
+
+    assert aggregator.totals.get('loggerCount') == 0
+  }
+
+  @Test
+  void muleFileHasSingleLoggerReference() {
+
+    def root = new XmlParser().parseText('''
+<mule xmlns:http="http://www.mulesoft.org/schema/mule/http"
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xsi:schemaLocation="
+http://www.mulesoft.org/schema/mule/http http://www.mulesoft.org/schema/mule/http/current/mule-http.xsd">
+  <logger />
+</mule>
+''')
+
+    aggregator.handleNode(new MuleXmlNode(root.children()[0], nodeChecker))
+
+    assert aggregator.totals.get('loggerCount') == 1
+  }
+}
