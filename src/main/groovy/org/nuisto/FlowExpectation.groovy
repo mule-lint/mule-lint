c26e7a4d1d55b1852bf7186135f73131667fc660
Chad Gorshing <gorshing@gmail.com>
2019-02-09 16:34:59 -0600

    Renamed FlowRule to FlowExpectation

---
+++ src/main/groovy/org/nuisto/FlowExpectation.groovy
@@ -0,0 +1,17 @@
+package org.nuisto
+
+class FlowExpectation {
+
+  FlowExpectation() {
+  }
+
+  FlowExpectation flows(String ignore) {
+    //The ignore parameter is "are"
+    return this
+  }
+
+  FlowExpectation camel(String ignore) {
+    //The "ignore" parameter is "cased"
+    return this
+  }
+}
