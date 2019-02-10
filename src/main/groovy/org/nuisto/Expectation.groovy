c83de7122fcd55ea31b4e23f95d4486a2cd99775
Chad Gorshing <gorshing@gmail.com>
2019-02-10 10:23:44 -0600

    Moved passing to the parent class

--- src/main/groovy/org/nuisto/Expectation.groovy
+++ src/main/groovy/org/nuisto/Expectation.groovy
@@ -4,14 +4,22 @@ import org.nuisto.model.Infraction
 
 abstract class Expectation {
 
+  boolean isPassing
+
   List<Infraction> infractions
 
   public void init() {
 
   }
 
-  //TODO I don't like this method name. It isn't descriptive to what is happening
-  //Need something that describes intent "beginningNewFile"
+  /**
+   * TODO I don't like this method name. It isn't descriptive to what is happening
+   * We have to have some notion of "resetting" based upon a new file.
+   *
+   * An expectation is based on a file, when we have a new file, then the expectation should be reset.
+   *
+   * Might turn this into more like event based "onNewFile", but I'm not sure about that.
+   */
   void reset() {
 
   }
